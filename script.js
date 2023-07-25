56f84f7e3e1832b055fa6dc3c647207db92a19fb
Administrator <deuanchambers94@gmail.com>
2023-07-24 21:32:31 -0400

    finished with logic now i just gotta style

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 //first creatre and array for movies
 const movies=[]
-
+console.log(movies)
 let renderContainer=document.querySelector(".renderContainer")
 const button= document.querySelector("button")
 const form = document.getElementById("addMovieForm")
+
 const addMovies=function(e){
     e.preventDefault()
     const genreSelect=document.querySelector("#genreSelect").value
     const movieName= document.querySelector("#movieTitle").value
-    if(!movieName){
+    if(!movieName && genreSelect==="all" ){
+        console.log("falsy")
         movieName.value=""
-    }else if(movieName !==""){
-        
+        renderMovies()
+    }else if(movieName!=="" && genreSelect==="all"){
+        renderMovies() 
+        console.log("this one")
+
+    }else if(movieName !==""&&genreSelect!=="all"){
+        console.log("not empty")
         movies.push({genre:genreSelect,movieTitle:movieName})
     }
-    renderMovies()
+   renderMovies(genreSelect)
    form.reset()
 
 }
 
-const renderMovies=function(){
+const renderMovies=function(genre){
 renderContainer.innerHTML=""
+
+ 
+
 movies.forEach(movieElement => {
     let divTag = document.createElement("div")
+    divTag.classList.add("movies")
         divTag.innerHTML= `<h1>${movieElement.movieTitle}</h1><p>${movieElement.genre}</p>`
-        console.log(divTag)
         renderContainer.appendChild(divTag);
 });
-filterMovies()
+
+if(genre!=="all"){
+    filterMovies()
+
+}
 }
 
 
@@ -48,5 +62,22 @@ filteredList.forEach(listItem => {
     divTag.innerHTML= `<h1>${listItem.movieTitle}</h1><p>${listItem.genre}</p>`
     renderContainer.appendChild(divTag);
 });
+
+
 }
+
+renderContainer.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("movies")){
+        deleteMovie(e)
+    }
+})
+
+const deleteMovie=function(e){
+let seletedMovie=e.target
+let movieContainer=renderContainer.children
+let index= Array.from(movieContainer).indexOf(seletedMovie)
+movies.splice(index,1)
+seletedMovie.remove()
+}
+
 button.addEventListener("click",addMovies)
